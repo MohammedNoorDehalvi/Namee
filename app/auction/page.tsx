aebc7001d552d4c2b173dc9c725226eae01f7a82
MohammedNoorDehalvi <mohammednoordehalvi96@gmail.com>
2026-05-19 11:22:04 +0530

    Fix captain auction redirect

--- app/auction/page.tsx
+++ app/auction/page.tsx
@@ -1,5 +1,18 @@
 import { LiveAuction } from '@/components/auction/LiveAuction';
+import { CaptainDashboardClient } from '@/components/captain/CaptainDashboardClient';
 
-export default function AuctionPage() {
+type AuctionPageProps = {
+  searchParams?: {
+    captain?: string;
+  };
+};
+
+export default function AuctionPage({ searchParams }: AuctionPageProps) {
+  const isCaptainMode = searchParams?.captain === '1';
+
+  if (isCaptainMode) {
+    return <CaptainDashboardClient />;
+  }
+
   return <LiveAuction mode="public" />;
 }
